db3bef2ee086fca0ae47e1aeb94c791481d2ff12
iris-ywang <iris_ywang@outlook.com>
2024-06-02 11:36:20 +0100

    can't fix teacher forcing GRU. Using normal GRU for now

--- chemvae/hyperparameters.py
+++ chemvae/hyperparameters.py
@@ -37,7 +37,7 @@ def load_params(param_file=None, verbose=True):
         'gru_depth': 4,
         'rnn_activation': 'tanh',
         'recurrent_dim': 50,
-        'do_tgru': True,                # use custom terminal gru layer 
+        'do_tgru': False,                # use custom terminal gru layer
         'terminal_GRU_implementation': 0, # use CPU intensive implementation; other implementation modes (1 - GPU, 2- memory) are not yet implemented
         'tgru_dropout': 0.0,
         'temperature': 1.00,            # amount of noise for sampling the final output 
